fa49eb522f2994b8c60ce413ee48657993826c76
Rotemy Yaari <vmalloc@gmail.com>
2025-08-06 19:20:25 +0300

    Fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -106,6 +106,9 @@ class Sha1Algorithm(Algorithm):
 class Md5Algorithm(Algorithm):
     pass
 
+class Blake3Algorithm(Algorithm):
+    pass
+
 
 class Sha256Algorithm(Algorithm):
     pass
@@ -115,7 +118,7 @@ class Sha512Algorithm(Algorithm):
     pass
 
 
-_ALL_ALGOS = (Sha1Algorithm, Md5Algorithm, Sha256Algorithm, Sha512Algorithm)
+_ALL_ALGOS = (Sha1Algorithm, Md5Algorithm, Sha256Algorithm, Sha512Algorithm, Blake3Algorithm)
 
 
 @pytest.fixture
